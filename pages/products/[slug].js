5e5100927dd0c538741538afa5054a1d7025d6c4
Abdul Rafay <rafayexalter@gmail.com>
2021-09-27 19:22:18 +0500

    Testing getServerSideProps

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -5,35 +5,9 @@ export default function ProductPage({ product }) {
   return <ProductPageContent product={product} />;
 }
 
-// export async function getServerSideProps({ params }) {
-//   const slug = params.slug;
-//   const product = await getProduct(slug);
-
-//   return {
-//     props: {
-//       product,
-//     },
-//   };
-// }
-
-export async function getStaticPaths() {
-  const products = await getAllProducts();
-
-  const paths = products.map((item) => {
-    const slug = String(item.node.handle);
-
-    // [slug].js
-    return { params: { slug } };
-  });
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({ params }) {
-  const product = await getProduct(params.slug);
+export async function getServerSideProps({ params }) {
+  const slug = params.slug;
+  const product = await getProduct(slug);
 
   return {
     props: {
@@ -41,3 +15,29 @@ export async function getStaticProps({ params }) {
     },
   };
 }
+
+// export async function getStaticPaths() {
+//   const products = await getAllProducts();
+
+//   const paths = products.map((item) => {
+//     const slug = String(item.node.handle);
+
+//     // [slug].js
+//     return { params: { slug } };
+//   });
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+
+// export async function getStaticProps({ params }) {
+//   const product = await getProduct(params.slug);
+
+//   return {
+//     props: {
+//       product,
+//     },
+//   };
+// }
